8fa1fdcccc01a3abb26091ed029c0bf63ede1abd
JailsonTech <jailsonrodriguestech@gmail.com>
2023-11-16 16:01:29 -0300

    Pastas iniciais

---
+++ Functions/myScript.js
@@ -0,0 +1,31 @@
+
+// ...DROPDOWN DO MOBILE...............
+function myFunction() {
+ document.getElementById("myDropdown").classList.toggle("mostrar");
+}
+
+window.onclick = function(e) {
+ if (!e.target.matches('.menuBar')) {
+ let myDropdown = document.getElementById("myDropdown");
+   if (myDropdown.classList.contains('mostrar')) {
+     myDropdown.classList.remove('mostrar');
+   }
+ }
+}
+// ...AVISO DE TRABALHANDO...............
+function aviso(){
+ alert("Aguarde!\n Estamos trabalhando"); 
+}
+// ...DROPDOWN DE +JOGOS...............
+function jogos(){
+  document.getElementById("maisJogos").classList.toggle('game');
+}
+window.onclick = function(e) {
+  if (!e.target.matches(".jogos")) {
+  let maisJogos = document.getElementById('maisJogos');
+    if (maisJogos.classList.contains('game')) {
+      maisJogos.classList.remove('game');
+    }
+  }
+ }
+ 
